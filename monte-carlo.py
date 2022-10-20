2bcc4de295b6531cecff591fb3807e75779f8949
Ellis Hewins <ellishewins@gmail.com>
2022-10-20 16:02:59 +0100

    Completed Monte Carlo task 2.

--- monte-carlo.py
+++ monte-carlo.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 
 def task1():
     uniform_rand = np.random.random_sample(10000)
@@ -12,7 +13,14 @@ def task1():
     plt.show()
 
 def task2():
-    print()
+    f = lambda x: np.exp(-np.abs(x))*np.cos(x)
+    lowerlim, upperlim = -np.pi/2, np.pi/2
+    N = 10000
+    V = upperlim - lowerlim
+    uniform_rand = lowerlim + V*np.random.random_sample(N)
+    I = V/N * np.sum(f(uniform_rand))
+    trueI,_ = integrate.quad(f, lowerlim, upperlim)
+    print("With a ransom sample of size {0:d}, the integral evaluated using Monte Carlo methods gives\nI = {1:.4f}, and when calculated numerically, gives I = {2:.4f}.".format(N, I, trueI))
 
 def task3():
     print()
