07a8eb5cfa0fd88e15c8eef8fa3d00b2152789ab
Ellis Hewins <ellishewins@gmail.com>
2022-12-09 22:29:30 +0000

    Added the the ability to plot 3D graphs of the bodies' paths, with code automatically plotting a graph appropriate to the number of dimensions. Also added a set of initial conditions which give a 4-body plot in 3 dimensions.

--- N_body_simulation/n-body.py
+++ N_body_simulation/n-body.py
@@ -1,22 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import Axes3D
 
-# Input variables for Burrau's problem
-G = 1 # Value of the gravitational constant
-pos = np.array([[1,-2,1],[3,-1,-1]]) # initial positions of the three bodies
-vel = np.array([[0,0,0],[0,0,0]]) # initial velocities of the three bodies
-m = np.array([3,4,5]) # masses of the three bodies
+# # Input variables for Burrau's problem
+# G = 1 # Value of the gravitational constant
+# pos = np.array([[1,-2,1],[3,-1,-1]]) # initial positions of the three bodies
+# vel = np.array([[0,0,0],[0,0,0]]) # initial velocities of the three bodies
+# m = np.array([3,4,5]) # masses of the three bodies
+# num_dimensions = pos.shape[0]
+# num_bodies = pos.shape[1]
+# t_tot = 70 # total time to run the simulation for
+# dt = 0.001 # time step between outputs calculated by odeint
+# t_vals = np.arange(0,t_tot+dt,dt) # Array time values to calcuclate the state of the system at.
+# plot_llim = 60
+# plot_ulim = 70
+# in_range = (t_vals >= plot_llim) & (t_vals <= plot_ulim)
+# rtol = 1e-23 # Tolerance settings used for odeint. Default values are 1.49012e-8
+# atol = 1e-10
+
+# Input variables for a 3 dimensional four body system
+G = 1
+m = np.array([1000,1,1,1]) # The central body should have a significantly greater mass than the other bodies in the system
+pos = np.array([[0,10,0,0],[0,0,20,0],[0,0,0,30]])
+# For this case, the velocity values are calculated based on the positions so as to give stable orbits
+vel = np.array([[0,0,0,np.sqrt(G*m[0]/pos[2,3])],[0,np.sqrt(G*m[0]/pos[0,1]),0,0],[0,0,np.sqrt(G*m[0]/pos[1,2]),0]])
 num_dimensions = pos.shape[0]
 num_bodies = pos.shape[1]
-t_tot = 70 # total time to run the simulation for
-dt = 0.001 # time step between outputs calculated by odeint
-t_vals = np.arange(0,t_tot+dt,dt) # Array time values to calcuclate the state of the system at.
-plot_llim = 60
-plot_ulim = 70
+t_tot = 50
+dt = 0.001
+t_vals = np.arange(0,t_tot+dt,dt)
+plot_llim = 0
+plot_ulim = t_tot
 in_range = (t_vals >= plot_llim) & (t_vals <= plot_ulim)
-rtol = 1e-23 # Tolerance settings used for odeint. Default values are 1.49012e-8
-atol = 1e-10
+rtol = 1.49012e-8
+atol = 1.49012e-8
 
 # # Input variables for the 2-body test case
 # G = 6.6748015e-11
@@ -28,6 +46,9 @@ atol = 1e-10
 # t_tot = 5 * 31557600
 # dt = 24*60*60
 # t_vals = np.arange(0,t_tot+dt,dt)
+# plot_llim = 0
+# plot_ulim = t_tot
+# in_range = (t_vals >= plot_llim) & (t_vals <= plot_ulim)
 # rtol = 1.49012e-8
 # atol = 1.49012e-8
 
@@ -41,6 +62,9 @@ atol = 1e-10
 # t_tot = 10 * 31557600
 # dt = 24*60*60
 # t_vals = np.arange(0,t_tot+dt,dt)
+# plot_llim = 0
+# plot_ulim = t_tot
+# in_range = (t_vals >= plot_llim) & (t_vals <= plot_ulim)
 # rtol = 1.49012e-8
 # atol = 1.49012e-8
 
@@ -59,12 +83,23 @@ def solve_func(X, t):
 
 solutions = odeint(solve_func, np.concatenate((pos,vel),axis=None), t_vals, rtol=rtol, atol=atol)
 
-plt.figure(1)
-plt.xlabel("$x$ coordinate")
-plt.ylabel("$y$ coordinate")
-for b in range(num_bodies):
-    plt.plot(solutions[:,b][in_range], solutions[:,b+num_bodies][in_range], label='Body {:d}'.format(b+1))
-plt.legend(loc='upper left')
+fig = plt.figure(1)
+if num_dimensions == 2:
+    ax = fig.add_subplot()
+    for b in range(num_bodies):
+        ax.plot(solutions[:,b][in_range], solutions[:,b+num_bodies][in_range], label='Body {:d}'.format(b+1))
+elif num_dimensions == 3:
+    ax = fig.add_subplot(projection='3d')
+    for b in range(num_bodies):
+        ax.plot(solutions[:,b][in_range], solutions[:,b+num_bodies][in_range], solutions[:,b+2*num_bodies][in_range], label='Body {:d}'.format(b+1))
+    ax.set_zlabel("$z$ coordinate")
+else:
+    print("Improper number of dimensions, must be 2D or 3D for data to be plotted.")
+    exit()
+
+ax.legend(loc='upper left')
+ax.set_xlabel("$x$ coordinate")
+ax.set_ylabel("$y$ coordinate")
 
 total_energy = 0.5 * np.sum(np.tile(m,num_dimensions)*solutions[:,num_bodies*num_dimensions:]**2, axis=1) # First calculate total the kinetic energy, since this can be done on one line
 for i in range(num_bodies): # Then add the gravitational potential energy by looping over each unique pair of bodies
