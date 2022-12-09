4311679f9615d195973bdbda988a8af49ab19452
Ellis Hewins <ellishewins@gmail.com>
2022-12-09 10:42:23 +0000

    Copied over improvements to the code from 3-body.py to n-body.py, and removed 3-body.py since it now simulates more than 3 bodies. Also removed inner loop from solve_func, optimising the code.

--- N_body_simulation/n-body.py
+++ N_body_simulation/n-body.py
@@ -2,59 +2,78 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-def grav_acc(i, r):
-    '''Function which when passed an index i as well as an array of N position coordinates corresponding to all the bodies in the simulation and a second array of each body's mass, returns the net gravitational acceleration of the object at index i in the array'''
-    G = 6.6748015e-11 # Value of the gravitational constant in Nm²kg⁻²
-    r_ij = r - np.reshape(r[:,i],(num_dimensions, 1))
-    r_ij = np.delete(r_ij, i, axis=1)
-    M = np.delete(m, i)
-    dists = np.sqrt(np.sum(r_ij**2, axis=0))
-    return G*np.sum(M*r_ij/dists**3, axis=1)
+# Input variables for Burrau's problem
+G = 1 # Value of the gravitational constant
+pos = np.array([[1,-2,1],[3,-1,-1]]) # initial positions of the three bodies
+vel = np.array([[0,0,0],[0,0,0]]) # initial velocities of the three bodies
+m = np.array([3,4,5]) # masses of the three bodies
+num_dimensions = pos.shape[0]
+num_bodies = pos.shape[1]
+t_tot = 70 # total time to run the simulation for
+dt = 0.001 # time step between outputs calculated by odeint
+t_vals = np.arange(0,t_tot+dt,dt) # Array time values to calcuclate the state of the system at.
+plot_llim = 60
+plot_ulim = 70
+in_range = (t_vals >= plot_llim) & (t_vals <= plot_ulim)
+rtol = 1e-23 # Tolerance settings used for odeint. Default values are 1.49012e-8
+atol = 1e-10
+
+# # Input variables for the 2-body test case
+# G = 6.6748015e-11
+# pos = np.array([[-0.5*1.496e11,0.5*1.496e11],[0,0]])
+# vel = np.array([[0,0],[-15*1e3,15*1e3]])
+# m = np.array([1.989e30, 1.989e30])
+# num_dimensions = pos.shape[0]
+# num_bodies = pos.shape[1]
+# t_tot = 5 * 31557600
+# dt = 24*60*60
+# t_vals = np.arange(0,t_tot+dt,dt)
+# rtol = 1.49012e-8
+# atol = 1.49012e-8
+
+# # Input variables for two light bodies and a heavy body - Earth, Mars, Sun
+# G = 6.6748015e-11
+# pos = np.array([[-1.496e11,-1.524*1.496e11,0],[0,0,0]])
+# vel = np.array([[0,0,0],[29.78*1e3,24.07*1e3,0]])
+# m = np.array([5.9722e24, 6.4171e23, 1.989e30])
+# num_dimensions = pos.shape[0]
+# num_bodies = pos.shape[1]
+# t_tot = 10 * 31557600
+# dt = 24*60*60
+# t_vals = np.arange(0,t_tot+dt,dt)
+# rtol = 1.49012e-8
+# atol = 1.49012e-8
 
 def solve_func(X, t):
-    X = np.reshape(X,(2,num_dimensions,num_bodies))
-    pos = np.array(X[0])
-    X[0] = X[1]
+    '''Function for solving the differential equations of the problem, for n bodies in n dimensions.'''
+    # NOTE: next thing to test is whether it's n dimensional yet.
+    drdt = X[num_dimensions*num_bodies:]
+    pos = X.reshape(2,num_dimensions,num_bodies)[0]
+    dvdt = np.zeros((num_dimensions,num_bodies))
     for i in range(num_bodies):
-        X[1,:,i] = grav_acc(i,pos)
-    return X.flatten()
+        r_ij = pos - np.reshape(pos[:,i],(num_dimensions, 1))
+        dists = np.sqrt(np.sum(r_ij**2, axis=0))
+        dists[i] = 1 # prevents a divide by zero error
+        dvdt[:,i] = G*np.sum(m*r_ij/dists**3, axis=1)
+    return np.concatenate((drdt, dvdt), axis=None)
 
-x1, x2 = -0.5 * 1.496e11, 0.5 * 1.496e11 # init. position components of bodies, in SI units
-y1, y2 = 0, 0
-vx1, vx2 = 0, 0 # init. velocity components of bodies, in SI units
-vy1, vy2 = -15 * 1e3, 15 * 1e3
-m1, m2 = 1.989e30, 1.989e30 # masses of the bodies, in SI units
-pos = np.array([[x1,x2],[y1,y2]])
-vel = np.array([[vx1,vx2],[vy1,vy2]])
-m = np.array([m1,m2])
-num_dimensions = np.shape(pos)[0]
-num_bodies = np.shape(pos)[1]
+solutions = odeint(solve_func, np.concatenate((pos,vel),axis=None), t_vals, rtol=rtol, atol=atol)
 
-t_tot = 5 * 31557600 # total time converted from years to seconds
-dt = 24*60*60 # size of the timestep
-t_vals = np.arange(0,t_tot+dt,dt) # Array of times
-# print(np.concatenate((pos,vel),axis=None))
-# print(solve_func(np.concatenate((pos,vel),axis=None), t_vals))
-solutions = odeint(solve_func, np.concatenate((pos,vel),axis=None), t_vals, full_output = 1)
-x1_vals = solutions[:,0]
-x2_vals = solutions[:,1]
-y1_vals = solutions[:,2]
-y2_vals = solutions[:,3]
+plt.figure(1)
+plt.xlabel("$x$ coordinate")
+plt.ylabel("$y$ coordinate")
+for b in range(num_bodies):
+    plt.plot(solutions[:,b][in_range], solutions[:,b+num_bodies][in_range], label='Body {:d}'.format(b+1))
+plt.legend(loc='upper left')
 
-fig = plt.figure(1,figsize=(14,5))
-plt.subplots_adjust(wspace=0.25)
-ax1 = fig.add_subplot(1,2,1)
-ax2 = fig.add_subplot(1,2,2)
-ax1.plot(x1_vals/1.496e11,y1_vals/1.496e11,'r-',label='Body 1')
-ax1.plot(x2_vals/1.496e11,y2_vals/1.496e11,'k-',label='Body 2')
-ax1.legend(loc='upper left')
-ax1.set_xlabel("$x$ [AU]")
-ax1.set_ylabel("$y$ [AU]")
-ax1.set_xlim(-.6,.6)
-ax1.set_ylim(-.4,.4)
-ax2.plot(t_vals/31557600, x1_vals/1.496e11, 'r-')
-ax2.plot(t_vals/31557600, x2_vals/1.496e11, 'k-')
-ax2.set_xlabel("$t$ [yr]")
-ax2.set_ylabel("$x$ [AU]")
+total_energy = 0.5 * np.sum(np.tile(m,num_dimensions)*solutions[:,num_bodies*num_dimensions:]**2, axis=1) # First calculate total the kinetic energy, since this can be done on one line
+for i in range(num_bodies): # Then add the gravitational potential energy by looping over each unique pair of bodies
+    for j in range(i+1,num_bodies):
+        total_energy += -G*m[i]*m[j]/np.sqrt(np.sum((solutions[:,j:num_bodies*num_dimensions:num_bodies]-solutions[:,i:num_bodies*num_dimensions:num_bodies])**2, axis=1))
+Delta_E = (total_energy - total_energy[0]) / total_energy[0]
+plt.figure(2)
+plt.xlabel("$t$")
+plt.ylabel("$\Delta E$")
+plt.plot(t_vals, Delta_E)
 
 plt.show()
